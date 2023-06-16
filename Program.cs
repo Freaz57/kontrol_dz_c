016e929051bff79c826be466888dd3cb94a663cd
Ilya <ok.brain@yandex.ru>
2023-06-16 22:35:47 +0300

    control

--- Program.cs
+++ Program.cs
@@ -54,29 +54,29 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
-Console.Write("Введите значение M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int akkerman(int m, int n)
-{
-if (m == 0) {
+// int akkerman(int m, int n)
+// {
+// if (m == 0) {
 
-return n + 1;
-}
+// return n + 1;
+// }
 
 
-else if (n == 0) 
-{
-    return akkerman(m - 1, 1);
-}
+// else if (n == 0) 
+// {
+//     return akkerman(m - 1, 1);
+// }
 
-else {
+// else {
 
-    return akkerman(m - 1, akkerman(m, n - 1));
+//     return akkerman(m - 1, akkerman(m, n - 1));
 
-    }
-}
+//     }
+// }
 
-Console.Write($"A(m,n) = {akkerman(m, n)} ");
+// Console.Write($"A(m,n) = {akkerman(m, n)} ");
